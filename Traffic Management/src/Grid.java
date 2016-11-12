2229cdeeb7acd4dc3f72bd0850df75da3a96c7ed
as1406 <as1406@gmail.com>
2016-11-12 18:41:41 -0500

    Acc & 2nd Sch.
    Acceleration is working fine
    Cars still overlaping
    2nd scheduling is done and working

--- Traffic Management/src/Grid.java
+++ Traffic Management/src/Grid.java
@@ -4,8 +4,7 @@ import java.util.Set;
 public class Grid {
 	
 	public Grid(int NumberOfStreets, int NumberOfAvenues, 
-			int MinBlockSide, int MaxBlockSide, 
-			int CarLength, int CarWidth, int Clearance) {
+			int MinBlockSide, int MaxBlockSide) {
 		if(NumberOfStreets >= 999 || NumberOfAvenues >= 999) {
 			System.out.println("Numbers are outside program capacity");
 			return;
@@ -13,27 +12,24 @@ public class Grid {
 		System.out.println("Creating the grid");
 		System.out.println("Initializing roads, entrance and exit points");
 		initRoads(NumberOfStreets, NumberOfAvenues, 
-				MinBlockSide, MaxBlockSide, CarLength, CarWidth, Clearance);
+				MinBlockSide, MaxBlockSide);
 		initControlPoints(Road.getEntrySet());
 	}
 	
 	private void initRoads(int NumberOfStreets, int NumberOfAvenues, 
-			int MinBlockSide, int MaxBlockSide, int CarLength, int CarWidth, 
-			int Clearance) {
+			int MinBlockSide, int MaxBlockSide) {
 		boolean initialized = Road.addRoads(NumberOfStreets, 
-				MinBlockSide*CarLength, MaxBlockSide*CarLength, 'S', 
-				CarWidth, Clearance);
+				MinBlockSide*Frame.carLength, MaxBlockSide*Frame.carLength, 'S');
 		if(!initialized)
 			System.out.println("Streets not initialized properly");
 		initialized = Road.addRoads(NumberOfAvenues, 
-				MinBlockSide*CarLength, MaxBlockSide*CarLength, 'A', 
-				CarWidth, Clearance);
+				MinBlockSide*Frame.carLength, MaxBlockSide*Frame.carLength, 'A');
 		if(!initialized)
 			System.out.println("Avenues not initialized properly");
 	}
 	
 	private void initControlPoints(Set<Map.Entry<char[] ,Road>> set) {
-		boolean initialized = TrafficPoint.addControlPoits(set);
+		boolean initialized = TrafficPoint.addControlPoints(set);
 		if(!initialized) {
 			System.out.println("Entrance and Exit Points and intersections"
 					+ " not initialized properly");
